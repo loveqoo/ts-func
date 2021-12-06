b5f97b8f38717f40d41be0eb8fc5f4385cf366be
Anthony <gunam.jung@gmail.com>
2021-12-07 00:19:04 +0900

    - 컴파일러 주석 추가

--- src/free.ts
+++ src/free.ts
@@ -38,6 +38,10 @@ class Trampoline<T> implements Compiler<T> {
   }
 }
 
+/**
+ * 사용자의 입력을 받으면 주입된 컴파일러를 생성하고 실행해준다.
+ * @param supplier  컴파일러를 생성하는 함수
+ */
 export const compiler = <A>(supplier: (context?: unknown) => Compiler<A>) => (
   program: FreeFlatMap<A>,
   context?: unknown
