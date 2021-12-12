ededfcfe97c26164c15f23c8327858ce8307dab3
Anthony <gunam.jung@gmail.com>
2021-12-12 19:32:02 +0900

    - semigroup, monoid 수정

--- src/free.ts
+++ src/free.ts
@@ -1,3 +1,8 @@
+/**
+ * 정의한 `Free Monad`를 분석하고 실행하는 컴파일러 인터페이스.
+ *
+ * @typeParam P 사용자의 입력 또는 이전 결과를 받아서 정의한 `Free Monad`를 리턴하는 함수
+ */
 export interface Compiler<P, T> {
   compile(f: P): (...args: unknown[]) => T;
 }
